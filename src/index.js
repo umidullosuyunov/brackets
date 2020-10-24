2d86a905bae85431884e2651487ebeeec8486bef
Umidullo Suyunov <suyunovumidullo@gmail.com>
2020-10-24 12:36:03 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const tracker = [];
+  const pairs = Object.fromEntries(bracketsConfig)
+  str.split``.map((s) => {
+    tracker.length && pairs[tracker.slice(-1)[0]] === s
+      ? tracker.pop()
+      : tracker.push(s);
+  });
+
+  return !tracker.length;
 }
